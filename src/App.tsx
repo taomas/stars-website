3fb71cf4efa4efaf89c3f2dd4cc6afc842ca4e0f
taomasliu <taomasliu@tencent.com>
2021-05-25 15:21:25 +0800

    fix: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+
 import Starry from './components/Starry'
 import Slider from './components/Slider'
 import Music from './components/Music'
 
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-      <Starry />
-      {/* {<Slider />} */}
+      <Router>
+        <Switch>
+          <Route exact path="/music/slider" component={Slider}></Route>
+          <Route exact path="/music/starry" component={Starry}></Route>
+          <Route exact path="*">
+            <Redirect to="/music/starry" />
+          </Route>
+        </Switch>
+      </Router>
       <Music />
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-// import Starry from './components/Starry'
-// import * as serviceWorker from "./serviceWorker";
+
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// serviceWorker.unregister();
